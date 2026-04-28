50c9dbe757e6fcc1ed2af72fc4330066ce5d066d
Pavan krishnan <pavankrishnan40@gmail.com>
2026-04-28 18:43:28 +0530

    train.py

--- train.py
+++ train.py
@@ -2,17 +2,17 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import cifar10
 
-# Load dataset
+
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-# Normalize
+
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Class names
+
 classes = ['airplane','automobile','bird','cat','deer',
            'dog','frog','horse','ship','truck']
 
-# CNN Model
+
 model = models.Sequential()
 
 model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
@@ -27,13 +27,13 @@ model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
 
-# Compile
+
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# Train
+
 model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
 
-# Save model
+
 model.save("model/cnn_model.h5")
